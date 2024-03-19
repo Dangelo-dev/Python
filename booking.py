6f3c2ffc923a8220da4365f93706d337d76cd5d6
Dangelo-dev <victordangelo00@gmail.com>
2024-03-19 13:08:05 -0300

    Alter: new function for interval aleatory and remove function for print x and y

--- booking.py
+++ booking.py
@@ -2,6 +2,7 @@ import pyautogui
 import threading
 import time
 import keyboard
+import random
 
 # Function to continuously monitor the "q" key press
 def check_q_key():
@@ -11,6 +12,12 @@ def check_q_key():
             stop_flag = True
             break
 
+# Function to interval aleatory
+interval_aleatory = random.uniform(1, 2)
+
+# Wait for interval aleatory
+time.sleep(interval_aleatory)
+
 # Start the thread for monitoring the "q" key press
 stop_flag = False
 q_thread = threading.Thread(target=check_q_key)
@@ -18,11 +25,8 @@ q_thread.start()
 
 # Main loop for mouse movements and clicks
 while not stop_flag:
-    x, y = pyautogui.position()
-    print("Coordenadas:", x, y)
-    
     pyautogui.moveTo(x=1025, y=964)
-    pyautogui.click(clicks=1, interval=3)
+    pyautogui.click(clicks=1, interval=interval_aleatory)
     
     time.sleep(0.1)  # Add a small delay to reduce CPU usage
 
